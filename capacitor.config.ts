2ccefa39951d9a7e5bca5c6abd19b4be5b345455
makarcovkirill-design <makarcovkirill@gmail.com>
2026-09-17 22:13:48 +0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,1 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
 
+const config: CapacitorConfig = {
+  appId: 'com.kyw3ll.studio',
+  appName: 'Kyw3ll Studio',
+  webDir: 'www'
+};
+
+export default config;
